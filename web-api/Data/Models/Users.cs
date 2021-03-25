de89a61af70c86642105a461371f97fe76d36f0f
rakzoHr <64655554+rakzoHr@users.noreply.github.com>
2021-03-24 18:43:07 -0600

    Added .gitignore and first data model

---
+++ web-api/Data/Models/Users.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web_api.Data.Models
+{
+    public class Users
+    {
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Country { get; set; }
+        public string JobTitle { get; set; }
+    }
+}
--- web-api/Startup.cs
+++ web-api/Startup.cs
@@ -30,7 +30,7 @@ namespace web_api
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "web_api", Version = "v1" });
+                c.SwaggerDoc("v2", new OpenApiInfo { Title = "web_api_updated", Version = "v2" });
             });
         }
 
@@ -41,7 +41,7 @@ namespace web_api
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "web_api v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v2/swagger.json", "web_api_updated v2"));
             }
 
             app.UseHttpsRedirection();
